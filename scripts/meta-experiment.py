ba1222dac8ee2037a8118a22a2a99febe50f6575
morgolt <mar.rodion@gmail.com>
2018-03-04 22:56:21 +0300

    Working version

--- scripts/meta-experiment.py
+++ scripts/meta-experiment.py
@@ -8,4 +8,5 @@ from lerot.experiment import MetaExperiment
 if __name__ == "__main__":
     experiment = MetaExperiment()
     # todo: fix config creation  in code (debug)
+    # todo: run summarization during meta experiment
     experiment.run()
