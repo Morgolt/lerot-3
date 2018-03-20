2f49daca4766865198f8c841fa16f8fee0155400
Rodion_Martynov <rodion_martynov@epam.com>
2018-03-20 14:17:42 +0300

    start implementation for multileaved comparisons

---
+++ lerot/system/WTALearningSystem.py
@@ -0,0 +1,12 @@
+from lerot.system.AbstractLearningSystem import AbstractLearningSystem
+
+
+class MultileaveLearningSystem(AbstractLearningSystem):
+    def get_ranked_list(self, query):
+        pass
+
+    def update_solution(self, clicks):
+        pass
+
+    def get_solution(self):
+        pass
